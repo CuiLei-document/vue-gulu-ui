481fd47e5d2057d7ab863330d754c6ec437258e8
磊哥 <493114008@qq.com>
2021-05-29 20:17:54 +0800

    将svg 封装成单文件 全局使用 Icon

--- src/button.vue
+++ src/button.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-        <button class="g-button ">按钮</button>
-    </div>
+        <button  class="g-button " :class="{[`icon-${classPrefix}`]:classPrefix}" >
+            <g-icon v-if="icon" class="icon" :name="icon"></g-icon>
+            <div class="content">
+                <slot />
+            </div>
+        </button>
 </template>
 <script>
  export default{
-
+    // props:['icon','classPrefix']
+     props:{
+         icon:{
+             type:String
+         },
+         classPrefix:{
+             type:String,
+             default:'left',
+             validator(value){
+                 return value === 'right' || value === 'left'
+             }
+         }
+     }
  }
+
 </script>
 
 <style lang="scss">
@@ -17,6 +33,10 @@
         border: 1px solid var(--border-color);
         padding: 0 1em;
         background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items:center;
+        vertical-align: middle;
         &:hover{
             border-color: var(--border-color);
         }
@@ -25,6 +45,25 @@
         }
         &:focus{
             outline: none;
+        }
+        & > .icon{
+            order: 1;
+            margin-top: 1px;
+            margin-right: .1em;
+        }
+        & > .content{
+            order: 2;
+        }
+        &.icon-right{
+            > .icon{
+                order: 2;
+                margin-right: 0;
+                margin-left: .1em;
+                margin-top: 1px;
+            }
+            > .content{
+                order: 1;
+            }
         }
     }
 
