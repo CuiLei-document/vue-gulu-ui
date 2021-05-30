3fd40b9586528a1cd1a2ab7077aab3c2a6c4d627
磊哥 <493114008@qq.com>
2021-05-30 17:59:23 +0800

    dist/index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-export  {default as Button} from './src/button.vue'
+
+export {default as Button} from './src/button'
 export {default as ButtonGroup} from './src/button-group'
-export {default as Icon} from './src/Icon'
+export {default as Icon} from './src/icon'
+
