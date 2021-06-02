c789362b9359e830129f51010a2c04738e0150ca
磊哥 <493114008@qq.com>
2021-06-02 14:28:44 +0800

    完成 点击 关闭事件

--- src/Toast.vue
+++ src/Toast.vue
@@ -1,11 +1,54 @@
 <template>
-    <div class="toast">
-        <slot/>
-    </div>
+    <transition name="fade">
+        <div class="toast" v-if="autoClose">
+            <slot></slot>
+            <span class="line"></span>
+            <span class="close" v-if="closeButton" @click="onCloseButton">
+                {{closeButton.text}}
+            </span>
+        </div>
+    </transition>
 </template>
 <script>
     export default {
-        name: 'GuluToast'
+        name: 'GuluToast',
+        props:{
+            autoClose:{
+                type:Boolean,
+                default:true
+            },
+            autoCloseDey:{
+                default:50
+            },
+            closeButton:{
+                type:Object,
+                default:()=>{
+                    return {
+                        text:'关闭',
+                        callback:function(toast){
+                            toast.close()
+                        }
+                    }
+                }
+            }
+        },
+        mounted(){
+            if(this.autoClose){
+                setTimeout(()=>{
+                    this.close()
+                },this.autoCloseDey * 1000)
+            }
+        },
+        methods:{
+            close(){
+                this.$el.remove()
+                this.$destroy()
+            },
+            onCloseButton(){
+                this.close()
+                this.closeButton.callback()
+            }
+        }
     }
 </script>
 <style lang="scss" scoped>
@@ -22,10 +65,27 @@
         transform: translateX(-50%);
         height:$toast-height;
         padding: 0 16px;
-        line-height:40px;
+        line-height:1.8;
+        display: flex;
+        align-items: center;
         border-radius: $border-radius;
         background: $toast-bg;
         color: white;
         box-shadow: $box-shadow;
+        > .line{
+            height: 100%;
+            border-left: 1px solid red;
+            margin-left: 16px;
+        }
+    }
+
+    .close{
+        margin-left: 16px;
+    }
+    .fade-enter-active,.fade-leave-active{
+        transition: all 3s;
+    }
+    .fade-enter,.fade-leave-to{
+        opacity: 0;
     }
 </style>
--- src/plugin.js
+++ src/plugin.js
@@ -3,8 +3,18 @@ export default{
     install(Vue,options){
         Vue.prototype.$toast = function(message){
             const Constructor = Vue.extend(Toast)
-            const toast = new Constructor()
+            const toast = new Constructor({
+               propsData:{
+                   closeButton:{
+                        text:'知道了',
+                        callback:function(){
+                            console.log('用户说知道了');
+                        }
+                   }
+               }
+            })
             toast.$slots.default = [message]
+
             toast.$mount()
             document.body.appendChild(toast.$el)
         }
